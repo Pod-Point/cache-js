4ffd87217618b14587d2f74fcff489bd992f16c4
Lee Brooks <leebrooks93@gmail.com>
2019-12-30 11:51:39 +0000

    Initial development
    * Bring code over from the `@pod-point/typescript` package
    * Update from TSLint to ESLint
    * Update README

---
+++ src/types/Expire.ts
@@ -0,0 +1,13 @@
+interface Expire {
+    /**
+     * The timestamp when the key/value pair should expire.
+     */
+    at?: number;
+
+    /**
+     * The number of seconds until the key/value pair should expire.
+     */
+    in?: number;
+}
+
+export default Expire;
