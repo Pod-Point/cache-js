cb49505d491fdce8a1d361d0b456b4efb774cefa
Martin Stuessy <martin.stuessy@pod-point.com>
2020-02-20 13:59:31 +0000

    add ephemeral mode tests to Redis.test.ts

---
+++ src/__tests__/services/redis/Redis.test.ts
@@ -0,0 +1,85 @@
+import Redis from '../../../services/Redis';
+import { createClient } from "redis";
+
+const mockSet = jest.fn((key, value, cb) => cb());
+const mockQuit = jest.fn();
+const mockDel = jest.fn();
+const mockExpire = jest.fn();
+const mockExpireAt = jest.fn();
+const mockGet = jest.fn((key, cb) => cb(null, 'someData'));
+
+jest.mock('redis', () => ({
+    createClient: jest.fn(() => ({
+        del: mockDel,
+        expire: mockExpire,
+        expireat: mockExpireAt,
+        get: mockGet,
+        set: mockSet,
+        quit: mockQuit,
+    })),
+}));
+
+describe('services/Redis', () => {
+    const key = 'foo';
+    const value = 'bar';
+
+    [true, false].forEach(ephemeral => {
+        const isEphemeral = ephemeral ? 'ephemeral' : 'nonEphemeral';
+        const cache = new Redis({}, ephemeral);
+
+        describe(isEphemeral, () => {
+
+            beforeEach(() => {
+                mockQuit.mockReset();
+            });
+
+            afterEach(() => {
+                if (ephemeral) {
+                    expect(createClient).toHaveBeenCalled();
+                    expect(mockQuit).toHaveBeenCalled();
+                }
+            });
+
+            it('persists the key/value pair in the cache and quits connection', async () => {
+                await cache.put(key, value);
+
+                expect(mockSet).toHaveBeenCalledWith('foo', 'bar', expect.any(Function));
+            });
+
+            it('persists the key/value pair in the cache to expire at a particular time and quits connection', async () => {
+                const timestamp = (new Date()).getTime();
+                await cache.put(key, value, {
+                    at: timestamp,
+                });
+
+                expect(mockSet).toHaveBeenCalledWith('foo', 'bar', expect.any(Function));
+                expect(mockExpireAt).toHaveBeenCalledWith(
+                    'foo',
+                    timestamp,
+                );
+            });
+
+            it('persists the key/value pair in the cache to expire in a given number of seconds and quits connection', async () => {
+                await cache.put(key, value, {
+                    in: 1,
+                });
+
+                expect(mockSet).toHaveBeenCalledWith('foo', 'bar', expect.any(Function));
+                expect(mockExpire).toHaveBeenCalledWith('foo', 1);
+            });
+
+            it('retrieves the keys value from the cache and quits connection', async () => {
+                const data = await cache.get(key);
+
+                expect(mockGet).toHaveBeenCalledWith('foo', expect.any(Function));
+                expect(data).toEqual('someData');
+            });
+
+            it('removes the key/value pair from the cache and quits connection', async () => {
+                await cache.remove(key);
+
+                expect(mockDel).toHaveBeenCalledWith('foo');
+            });
+        });
+    });
+});
